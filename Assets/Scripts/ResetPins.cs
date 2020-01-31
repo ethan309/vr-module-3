19f5a20526638be8fd1adce0bd4863c80b44b2ee
Ethan Butt <ethanbutt309@gmail.com>
2020-01-31 17:04:51 -0600

    Setup for game environment.

---
+++ Assets/Scripts/ResetPins.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ResetPins : MonoBehaviour
+{
+    private GameObject[] pins;
+
+    public int winningScore;
+    public int score;
+
+    public Text scoreDisplayText;
+    public Text endgameMessage;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pins = GameObject.FindGameObjectsWithTag("Pin");
+        endgameMessage.text = "";
+    }
+
+    void UpdateScore(int newScore)
+    {
+        score = newScore;
+        scoreDisplayText.text = "Score: " + score.ToString();
+        if (score >= winningScore)
+        {
+            endgameMessage.text = "Please Reset Pins";
+        }
+        else
+        {
+            endgameMessage.text = "";
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        int newScore = 0;
+        foreach (GameObject pin in pins)
+        {
+            if (pin.GetComponent<Resetable>().hit)
+            {
+                newScore += 1;
+            }
+        }
+        UpdateScore(newScore);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Triggerred");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            score = 0;
+            foreach (GameObject pin in pins)
+            {
+                pin.GetComponent<Resetable>().ToggleReset(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SnapToPoint.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class SnapToPoint : MonoBehaviour, Resetable
+    {
+        private bool shouldReset;
+        private Vector3 snapTo;
+        private Quaternion snapAngle;
+        private Rigidbody body;
+        public float snapTime = 2;
+
+        private float dropTimer;
+        private Interactable interactable;
+
+        bool Resetable.hit => false;
+
+        public void ToggleReset(bool status)
+        {
+            shouldReset = status;
+        }
+
+        private void Start()
+        {
+            snapTo = transform.TransformDirection(transform.position);
+            snapAngle = transform.rotation;
+            shouldReset = false;
+            interactable = GetComponent<Interactable>();
+            body = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            bool used = false;
+
+            if (interactable != null)
+                used = interactable.attachedToHand;
+
+            if (used || transform.position == snapTo && transform.rotation == snapAngle)
+            {
+                body.velocity = Vector3.zero;
+                body.angularVelocity = Vector3.zero;
+                body.isKinematic = false;
+                dropTimer = -1;
+                shouldReset = false;
+            }
+            else if (shouldReset)
+            {
+                dropTimer += Time.deltaTime / (snapTime / 2);
+
+                body.isKinematic = dropTimer > 1;
+
+                // Already back in target position
+                if (dropTimer > 1)
+                {
+                    //transform.parent = snapTo;
+                    transform.position = snapTo;
+                    transform.rotation = snapAngle;
+                    shouldReset = false;
+                    dropTimer = -1;
+                }
+                // Still returning to target position
+                else
+                {
+                    float t = Mathf.Pow(35, dropTimer);
+
+                    body.velocity = Vector3.Lerp(body.velocity, Vector3.zero, Time.fixedDeltaTime * 4);
+                    if (body.useGravity)
+                        body.AddForce(-Physics.gravity);
+
+                    transform.position = Vector3.Lerp(transform.position, snapTo, Time.fixedDeltaTime * t * 3);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, snapAngle, Time.fixedDeltaTime * t * 2);
+                }
+            }
+        }
+    }
+}
