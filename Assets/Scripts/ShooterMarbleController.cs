361e3097dad0534bfe2933788bbd8be1a162c01d
Ethan Butt <ethanbutt309@gmail.com>
2020-02-05 16:28:01 -0600

    styling

---
+++ Assets/Scripts/ShooterMarbleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShooterMarbleController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Target Object"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
